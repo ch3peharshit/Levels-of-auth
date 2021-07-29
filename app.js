dca529fb52769e84c7c4baabdbe403c735c2519b
Harshit Chepe <hcgolucool8@gmail.com>
2021-07-30 03:06:43 +0530

    Level 1 Authentication - Username and password login / register with redirection to secrets viewpage

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser:true,useUnifiedTopology:true})
+const app = express()
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true
+}))
+app.set('view engine','ejs')
+
+const userSchema = mongoose.Schema({
+    email:String,
+    password:String
+})
+const User = new mongoose.model("user",userSchema)
+app.get('/',function(req,res){
+    res.render('home')
+})
+app.get('/login',function(req,res){
+    res.render('login')
+})
+app.get('/register',function(req,res){
+    res.render('register')
+})
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        } else {
+            res.render("secrets")
+        }
+    })
+})
+app.post('/login',function(req,res){
+    const username =req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username},(err,found)=>{
+        if(err){
+            console.log(err)
+        } else {
+            if(found){
+                if(found.password === password){
+                    res.render('secrets')
+                } else {
+                    res.send("WRONG PASSWROD")
+                }
+            }
+        }
+    })
+})
+app.listen('3000',function(){
+    console.log("running on 3000")
+})
