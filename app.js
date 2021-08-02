e1a5d05be3d510d7f63d93d2ae054873675fb5d8
Harshit Chepe <hcgolucool8@gmail.com>
2021-08-03 04:23:26 +0530

    Added google OAuth 2.0 for authentication (login/register), Users can submit their secrets to a global secrets page.

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const  session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
 //const encrypt = require('mongoose-encryption')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const findOrCreate = require('mongoose-findorcreate')
 const app = express()
 
 app.use(session({
@@ -30,19 +32,50 @@ app.set('view engine','ejs')
 
 const userSchema = new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String,
+    Secrets:String
 })
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 //userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']})
 const User = new mongoose.model("user",userSchema)
 
-passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+//passport.use(User.createStrategy())
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret:process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken,refreshToken,profile,cb){
+    
+    User.findOrCreate({googleId:profile.id},function(err,user){
+        return cb(err,user);
+    })
+
+}
+))
+passport.serializeUser(function(user,done){
+    done(null,user.id)
+})
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        done(err,user)
+    })
+})
 
 app.get('/',function(req,res){
     res.render('home')
+})
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+app.get('/auth/google/secrets',
+passport.authenticate('google',{failureRedirect:'/login'}),function(req,res){
+    res.redirect('/secrets')
 })
 app.get('/login',function(req,res){
     res.render('login')
@@ -116,16 +149,57 @@ app.post('/login',function(req,res){
     })
 })
 app.get('/secrets',function(req,res){
-    if(req.isAuthenticated()){
-        res.render("secrets")
-    } else {
-        res.redirect('/login')
-    }
+    // if(req.isAuthenticated()){
+    //     User.findById(req.user.id,function(err,found){
+    //         if(!err){
+    //             if(found){
+
+    //             }
+    //         } else {
+    //             console.log("no secrets found")
+    //         }
+    //     })
+    //     res.render("secrets")
+    // } else {
+    //     res.redirect('/login')
+    // }
+    User.find({"Secrets":{$ne:null}},function(err,found){
+        if(err){
+            console.log(err)
+        } else {
+            if(found){
+                res.render("secrets",{userWithSecrets:found})
+            }
+        }
+    })
     
 })
 app.get('/logout',function(req,res){
     req.logout()
     res.redirect('/')
+})
+app.get('/submit',function(req,res){
+    if(req.isAuthenticated()){
+        res.render('submit')
+    } else {
+        res.redirect('/login')
+    }
+})
+app.post('/submit',function(req,res){
+    const secretSubmitted = req.body.secret;
+    
+    User.findById(req.user.id,function(err,found){
+        if(!err){
+            if(found){ 
+                found.Secrets = secretSubmitted;
+                found.save(function(){
+                    res.redirect("/secrets")
+                })
+            } 
+        } else {
+            console.log("error")
+        }
+    })
 })
 app.listen('3000',function(){
     console.log("running on 3000")
